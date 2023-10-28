2070d117d24412c6168ffc9ae7dde41295c7f27a
Script Raccoon <scriptraccoon@gmail.com>
2023-10-29 00:54:50 +0200

    improve perf display

--- surjective_counting.py
+++ surjective_counting.py
@@ -12,7 +12,7 @@ from time import perf_counter
 def surject1(m: int, n: int) -> int:
     """number of surjective maps [m] -> [n] where [n] is a set
     with n elements, recursive implementation"""
-    if n == 0:
+    if not n:
         return int(m == 0)
     return sum(comb(m, k) * surject1(k, n - 1) for k in range(m))
 
@@ -23,7 +23,7 @@ def surject2(m: int, n: int) -> int:
     with n elements, counted with inclusion-exclusion principle"""
     if n > m:
         return 0
-    if m == 0:
+    if not m:
         return 1
     return sum((-1) ** i * comb(n, i) * (n - i) ** m for i in range(n))
 
@@ -42,8 +42,22 @@ def measure_fun(fun, msg="") -> None:
 
 def main() -> None:
     """measure performance of the two implementations"""
-    measure_fun(surject1, msg="Test recursive implementation")
-    measure_fun(surject2, msg="Test inclusion-exclusion implementation")
+    values = [(10, 5), (10, 2), (20, 1), (20, 10), (20, 15), (20, 20)]
+    for m, n in values:
+        print("recursive implemention")
+        start1 = perf_counter()
+        surject1(m, n)
+        end1 = perf_counter()
+        time1 = end1 - start1
+        print(f"{m},{n}: {time1}")
+        print("inclusion-exclusion implemention")
+        start2 = perf_counter()
+        surject2(m, n)
+        end2 = perf_counter()
+        time2 = end2 - start2
+        print(f"{m},{n}: {time2}")
+        print("ratio=", time1 / time2)
+        print()
 
 
 if __name__ == "__main__":

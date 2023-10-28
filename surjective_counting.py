bc4c0f75ea412455a53e5054d2bd28427296f6d6
Script Raccoon <scriptraccoon@gmail.com>
2023-10-29 00:49:51 +0200

    extract testing

--- surjective_counting.py
+++ surjective_counting.py
@@ -28,30 +28,20 @@ def surject2(m: int, n: int) -> int:
     return sum((-1) ** i * comb(n, i) * (n - i) ** m for i in range(n))
 
 
-def test_equality(limit: int = 15) -> None:
-    """tests if the two implementations compute the same"""
-    print("Test if the two implementations compute the same")
-    for m in range(limit):
-        for n in range(m):
-            assert surject1(m, n) == surject2(m, n)
-    print("OK")
-
-
 def measure_fun(fun, msg="") -> None:
     """measures performance of a function"""
     print(msg)
-    values = [(10, 5), (10, 2), (20, 10), (20, 20), (20, 1), (25, 25)]
+    values = [(10, 5), (10, 2), (20, 10), (20, 20), (20, 1)]
     for m, n in values:
         start = perf_counter()
         fun(m, n)
         end = perf_counter()
         time = end - start
-        print(m, n, ": ", time)
+        print(f"{m},{n}: {time}")
 
 
 def main() -> None:
     """measure performance of the two implementations"""
-    test_equality()
     measure_fun(surject1, msg="Test recursive implementation")
     measure_fun(surject2, msg="Test inclusion-exclusion implementation")
 
---
+++ test_counting.py
@@ -0,0 +1,10 @@
+"""Tests for surjective_counting.py"""
+
+from surjective_counting import surject1, surject2
+
+
+def test_counting():
+    """tests if the two implementations compute the same"""
+    for m in range(15):
+        for n in range(m):
+            assert surject1(m, n) == surject2(m, n)
